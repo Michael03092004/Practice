caed5408aeba6d71149e3ab683c127b67d080f7d
Misha Marchenko <netproblem394@gmail.com>
2023-11-25 10:28:55 +0200

    Updated a little bit

--- PZ_5.3/src/main/java/org/example/Main.java
+++ PZ_5.3/src/main/java/org/example/Main.java
@@ -4,10 +4,13 @@ import org.example.First.PrintServer;
 import org.example.First.Printer;
 import org.example.First.User;
 import org.example.Second.BlockingStack;
+import org.example.Third.TestThreadState;
 
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.Stack;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
@@ -15,7 +18,9 @@ import java.util.stream.IntStream;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    static volatile boolean canDestroyPutin;
+
+    public static int a = 0;
+    private static final Object lock = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         //First task
@@ -64,50 +69,55 @@ public class Main {
 
         //third task
 
-        Object lock = new Object();
+
+        Thread thread = new Thread(() -> {
+            synchronized (lock) {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        System.out.println("Thread state: " + thread.getState());
+
+        thread.start();
+
+        System.out.println("Thread state: " + thread.getState());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread state: " + thread.getState());
 
         Thread blockedThread = new Thread(() -> {
             synchronized (lock) {
-                System.out.println("Blocked Thread: Acquiring lock");
-                try {
-                    // Simulate some work
-                    Thread.sleep(1000);
-                    System.out.println("Blocked Thread: Waiting for a notification");
-                    lock.wait(); // Thread will be blocked here until notified
-                    System.out.println("Blocked Thread: Resumed after notification");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
         });
 
-        Thread notifyingThread = new Thread(() -> {
-            synchronized (lock) {
-                System.out.println("Notifying Thread: Acquiring lock");
-                // Simulate some work
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Notifying Thread: Notifying the blocked thread");
-                lock.notify(); // Notifying the blocked thread
-            }
-        });
-        //NEW state for blockedThread cause it wasn't run
-        System.out.println(blockedThread.getState());
         blockedThread.start();
-        //RUNNABLE state for blockedThread cause it was started
-        System.out.println(blockedThread.getState());
-        Thread.sleep(2);
-        notifyingThread.start();
 
-        //TIME_WAITING state for blockedThread cause Thread is sleeping for 2 milliseconds
-        System.out.println(blockedThread.getState());
-        //Obviously RUNNABLE
-        System.out.println(notifyingThread.getState());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println("Thread state: " + blockedThread.getState());
 
+        try {
+            thread.join();
+            blockedThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println("Thread state: " + thread.getState());
     }
 }
